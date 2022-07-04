0c9f6bc734ef3f9ba5278cf7d8489103f5b77e71
KhoiDo-CV <108678020+KhoiDo-CV@users.noreply.github.com>
2022-07-04 21:05:19 +0700

    uploads

---
+++ js/nicescroll/nicescroll.js
@@ -0,0 +1,17 @@
+/*$(function(){
+	$("html").niceScroll({
+		cursorborder: "none",
+		cursorborderradius: "0",
+		cursorwidth: "8px",
+		cursorcolor: "#FABC03",
+		cursoropacitymin: 0.8,
+		cursoropacitymax: 1,
+		background: "rgba(255,255,255,0.2)",
+		scrollspeed: 60,
+		mousescrollstep: 80,
+		autohidemode: true,
+		zindex: "9999999"	
+	});
+})*/
+
+
